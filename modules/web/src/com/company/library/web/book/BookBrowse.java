6979e45e9309d2e09eb9384863b46a7db1817e7a
henohgn@mail.ru <#xlibri5>
2018-10-28 07:56:04 +0800

    build.gradle

--- modules/web/src/com/company/library/web/book/BookBrowse.java
+++
@@ -1,6 +0,0 @@
-package ml.exlibris.exlibris.web.book;
-
-import com.haulmont.cuba.gui.components.AbstractLookup;
-
-public class BookBrowse extends AbstractLookup {
-}
--- modules/web/src/com/company/library/web/book/BookEdit.java
+++
@@ -1,7 +0,0 @@
-package ml.exlibris.exlibris.web.book;
-
-import com.haulmont.cuba.gui.components.AbstractEditor;
-import ml.exlibris.exlibris.entity.Book;
-
-public class BookEdit extends AbstractEditor<Book> {
-}
