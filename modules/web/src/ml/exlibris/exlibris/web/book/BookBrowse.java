d1678702274581e7f9f33168a94716ecda55b0ce
henohgn@mail.ru <#xlibri5>
2018-10-28 08:27:53 +0800

    renamings

---
+++ modules/web/src/ml/exlibris/exlibris/web/book/BookBrowse.java
@@ -0,0 +1,6 @@
+package ml.exlibris.exlibris.web.book;
+
+import com.haulmont.cuba.gui.components.AbstractLookup;
+
+public class BookBrowse extends AbstractLookup {
+}
---
+++ modules/web/src/ml/exlibris/exlibris/web/book/BookEdit.java
@@ -0,0 +1,7 @@
+package ml.exlibris.exlibris.web.book;
+
+import com.haulmont.cuba.gui.components.AbstractEditor;
+import ml.exlibris.exlibris.entity.Book;
+
+public class BookEdit extends AbstractEditor<Book> {
+}
